7747ed9caaf2620c9ab786cb8626f3473805a6bf
EdsonJSF <edsonjsf1990@gmail.com>
2023-05-31 00:26:10 -0400

    Update: Front App - Routes and lazyload

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './auth/guards/auth.guard';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'post',
+    loadChildren: () => import('./post/post.module').then((m) => m.PostModule),
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'post',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front/src/app/post/post-routing.module.ts
+++ front/src/app/post/post-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+import { AllPostsComponent } from './pages/all-posts/all-posts.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { MyPostsComponent } from './pages/my-posts/my-posts.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: MyPostsComponent },
+      { path: 'all', component: AllPostsComponent },
+      { path: 'create', component: CreatePostComponent },
+      { path: '**', redirectTo: '' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PostRoutingModule { }
+export class PostRoutingModule {}
